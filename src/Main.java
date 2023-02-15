69c7efde3846720e9a893352fbee0c1ac94726e1
dryg <xboxseriaskell@gmail.com>
2023-02-15 14:03:22 +0500

    тестовое написание переменных по видео

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var box = 5;
+        System.out.println(box);
+        box = box + 2;
+        System.out.println(box);
+box = box - 3;
+        System.out.println(box);
     }
 }
