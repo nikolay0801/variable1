3e61390683377192b98c8785ec7db2f2232ca2b5
dryg <xboxseriaskell@gmail.com>
2023-02-15 14:17:39 +0500

    задачи 1- 3 ( решение ) на проверку кода

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        var box = 5;
-        System.out.println(box);
-        box = box + 2;
-        System.out.println(box);
-box = box - 3;
-        System.out.println(box);
+        var dog =8.0;
+        System.out.println(dog);
+var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+        dog = dog + 4;
+        System.out.println(dog);
+        cat = cat + 4;
+        System.out.println(cat);
+        paper = paper + 4;
+        System.out.println(paper);
+        dog = dog - 3.5;
+        System.out.println(dog);
+        cat = cat - 1.6;
+        System.out.println(cat);
+        paper = paper - 7639;
+        System.out.println(paper);
+
+
+
+
+
+
     }
 }
