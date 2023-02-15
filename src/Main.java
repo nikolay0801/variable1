3476df482d834fc9bfb50d9d82be3bcc71b40973
dryg <xboxseriaskell@gmail.com>
2023-02-15 14:51:05 +0500

    задача 4 ( решение ) на проверку кода

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        var box = 5;
-        System.out.println(box);
-        box = box + 2;
-        System.out.println(box);
-box = box - 3;
-        System.out.println(box);
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend * 2;
+        System.out.println(friend);
+        friend = friend / 3;
+        System.out.println(friend);
+
     }
 }
